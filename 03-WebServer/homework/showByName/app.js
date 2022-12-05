f0bc0aa6b4ece2607b0c79120a2981c0f9414bd0
adrianavinicki <101589203+adrianavinicki@users.noreply.github.com>
2022-12-04 21:17:07 -0300

    Add files via upload

---
+++ 03-WebServer/homework/showByName/app.js
@@ -0,0 +1,18 @@
+var fs  = require("fs");
+var http  = require("http");
+
+// Escribí acá tu servidor
+http.createServer( function(req, res){
+    fs.readFile(`./images${req.url}.jpg`, function(err,data){
+        if(err){
+            res.writeHead(404, {'Content-Type': 'text/plain'})
+            res.end('image not found')
+        }
+        else{
+            res.writeHead(200, {'Content-Type': 'image/jpg'} )
+            res.end(data)
+        }
+    })
+    
+}).listen(3000, '127.0.0.1');
+
